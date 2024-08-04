7d9186f4382132084ecb4c00902d64572f10acee
Bhaskar Mahto <arbkm22@gmail.com>
2024-08-05 00:39:29 +0530

    top comp added

--- src/App.js
+++ src/App.js
@@ -1,54 +1,112 @@
-import logo from './logo.svg';
 import './App.css';
 import bitcoin from './assets/bitcoin.png';
 import Dashboard from './components/Dashboard/Dashboard';
+import { useEffect, useState } from 'react';
+
+const API_KEY = process.env.REACT_APP_COINS_API_KEY;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <span className="logo">
-            <img className="logo-img" src={bitcoin} alt="bitcoin symbol" />
-        </span>
-        <span className="title">Coin Tracker</span>
-      </header> 
-      <div className="body">
-        <span className="main-text">
-            Crypto Prices by Market Cap
-        </span>
-        <span className="below-text">
-            The worldwide cryptocurrency market capitalization today stands at an 
-            estimated ₹212.5T , seeing a 1.81% movement over the last 24 hours. 
-            The total cryptocurrency trading volume in the past day is roughly ₹6T. 
-            Bitcoin's market dominance is at about 53.1%.
-        </span>    
-        <div className="block-container">
-            <div className="blocks one">
-                <div className="box-text">
-                    <span className="box-title">Market Cap</span>
-                    <span className="box-content">₹209,060,685,209,763.62</span>
-                    <span className="box-box box-red">⮟ 0.96%</span>
-                </div>
-            </div>
-            <div className="blocks two">
-                <div className="box-text">
-                    <span className="box-title">Volume</span>
-                    <span className="box-content">₹7,682,975,788,227.84</span>
-                    <span className="box-box box-green">⮝ 27.8%</span>
-                </div>
-            </div>
-            <div className="blocks three">
-                <div className="box-text">
-                    <span className="box-title">BTC Dominance</span>
-                    <span className="box-content">53.3%</span>
-                    <span className="box-box box-green">⮝ 0.17%</span>
-                </div>
+
+    const [marketData, setMarketData] = useState({});
+    const formatData = (data) => {
+        console.log('volumechange: ', data.volumeChange);
+        if (data.volumeChange < 0) {
+            console.log('less');
+        } else {
+            console.log('more');
+        }
+
+        return data;
+    }
+
+    const fetchMarketData = async () => {
+        try {
+            const marketEndpoint = 'https://openapiv1.coinstats.app/markets';
+            const response = await fetch(marketEndpoint, {
+                method: 'GET',
+                headers: {
+                    "X-API-KEY": API_KEY
+                },
+            });
+
+            if (!response.ok) {
+                console.log('Network error: ');
+            }
+
+            const data = await response.json();
+            setMarketData(formatData(data));
+            
+        } catch (error) {
+            console.log('error: ', error);
+        }
+    };
+
+    useEffect(() => {
+        fetchMarketData();
+    }, []);
+
+    return (
+        <div className="App">
+        <header className="App-header">
+            <span className="logo">
+                <img className="logo-img" src={bitcoin} alt="bitcoin symbol" />
+            </span>
+            <span className="title">Coin Tracker</span>
+        </header> 
+        <div className="body">
+            <span className="main-text">
+                Crypto Prices by Market Cap
+            </span>
+            <span className="below-text">
+                The worldwide cryptocurrency market capitalization today stands at an 
+                estimated ₹212.5T , seeing a 1.81% movement over the last 24 hours. 
+                The total cryptocurrency trading volume in the past day is roughly ₹6T. 
+                Bitcoin's market dominance is at about 53.1%.
+            </span>    
+            <div className="block-container">
+                {marketData && (
+                    <>
+                        <div className={`blocks ${marketData.marketCapChange < 0 ? 'block-red' : 'block-green'}`}>
+                            <div className="box-text">
+                                <span className="box-title">Market Cap</span>
+                                <span className="box-content">₹{marketData.marketCap}</span>
+                                {/* <span className="box-box box-red">⮟ {marketData.marketCapChange}%</span> */}
+                                <span className={`box-box ${marketData.marketCapChange < 0 ? 'box-red' : 'box-green'}`}>
+                                    {marketData.marketCapChange < 0 ? '⮟' : '⮝'}
+                                    {marketData.marketCapChange}%
+                                </span>
+                            </div>
+                        </div>
+                        <div className={`blocks ${marketData.volumeChange < 0 ? 'block-red' : 'block-green'}`}>
+                            <div className="box-text">
+                                <span className="box-title">Volume</span>
+                                <span className="box-content">₹{marketData.volume}</span>
+                                {/* <span className="box-box box-green">⮝ {marketData.volumeChange}%</span> */}
+                                <span className={`box-box ${marketData.volumeChange < 0 ? 'box-red' : 'box-green'}`}>
+                                    {marketData.volumeChange < 0 ? '⮟' : '⮝'}
+                                    {marketData.volumeChange}%
+                                </span>
+                            </div>
+                        </div>
+                        <div className={`blocks ${marketData.btcDominanceChange < 0 ? 'block-red' : 'block-green'}`}>
+                            <div className="box-text">
+                                <span className="box-title">BTC Dominance</span>
+                                <span className="box-content">{marketData.btcDominance}%</span>
+                                {/* <span className="box-box box-green">⮝ {marketData.btcDominanceChange}%</span> */}
+                                <span className={`box-box ${marketData.btcDominanceChange < 0 ? 'box-red' : 'box-green'}`}>
+                                    {marketData.btcDominanceChange < 0 ? '⮟' : '⮝'}
+                                    {marketData.btcDominanceChange}%
+                                </span>
+                            </div>
+                        </div>
+                    </>
+                )}
+                
             </div>
         </div>
-      </div>
-      <Dashboard />
-    </div>
-  );
+        <Dashboard />
+        </div>
+    );
 }
 
 export default App;
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,10 +1,23 @@
-import React from "react";
-import './Dashboard.css'
+import React, { useState, useEffect } from "react";
+import './Dashboard.css';
 
 function Dashboard() {
+
+    function getCoinData() {
+        
+    }      
+
     return (
         <div className="dashboard">
-            Dashboard
+            <div className="nos">#</div>
+            <div className="name">Name</div>
+            <div className="1h common">1h%</div>
+            <div className="24h common">24h%</div>
+            <div className="7d common">7d%</div>
+            <div className="price common">Price</div>
+            <div className="market-cap common">Market Cap</div>
+            <div className="volume-24h common">Volume 24h</div>
+            <div className="price-graph common">Price Graph</div>
         </div>
     );
 }
